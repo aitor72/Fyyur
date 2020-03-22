306ed7fbb1bf4ff24a22d9875262ce9e91afdbe5
Aitor <soy@aitor.me>
2020-03-22 20:01:11 +0100

    Conect to db in app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from flask_migrate import Migrate
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -22,6 +23,9 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 # TODO: connect to a local postgresql database
+migrate = Migrate(app,db)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://postgres:root@localhost:5432/fyyur"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #----------------------------------------------------------------------------#
 # Models.
--- config.py
+++ config.py
@@ -10,4 +10,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
- SQLALCHEMY_DATABASE_URI = "postgres://postgres:root@localhost:5432/fyyur"
+SQLALCHEMY_DATABASE_URI = "postgres://postgres:root@localhost:5432/fyyur"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
